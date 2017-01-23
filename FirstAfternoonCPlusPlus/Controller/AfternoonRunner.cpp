4fcfc48480d47d269653e96d07c8789b7ad174ef
spal7580 <spal7580@740s-pl-131.local>
2017-01-23 13:31:20 -0700

    Update

---
+++ FirstAfternoonCPlusPlus/Controller/AfternoonRunner.cpp
@@ -0,0 +1,17 @@
+//
+//  AfternoonRunner.cpp
+//  FirstAfternoonCPlusPlus
+//
+//  Created by Paladugu, Sai neeraj on 1/23/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+#include "FirstController.h"
+
+int main()
+{
+    FirstController * app = new FirstController();
+    app->start();
+
+    return 0; 
+}
